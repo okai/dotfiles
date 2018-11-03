c8a994642859d4ad7153da298e007353a76999d5
okai <okai8@me.com>
2018-11-03 14:35:58 +0900

    add .tmux.conf and add link item setting on setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ ln -sf ./dotfiles/.zprofile ~/.zprofile
 ln -sf ./dotfiles/.zshrc ~/.zshrc
 ln -sf ./dotfiles/.zshenv ~/.zshenv
 ln -sf ./dotfiles/.vimperatorrc ~/.vimperatorrc
+ln -sf ./dotfiles/.tmux.conf ~/.tmux.conf
